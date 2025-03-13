43ffb6543508915be4facde77287903b652ad4db
Ivan Aslanov <ivan.aslanov@playwing.net>
2025-03-13 20:51:51 +0200

    Added the date of the external request when it was done

--- src/Logger/ExternalRequest.php
+++ src/Logger/ExternalRequest.php
@@ -4,18 +4,24 @@ declare(strict_types=1);
 
 namespace Bmwx591\RequestLogger\Logger;
 
+use DateTimeImmutable;
+
 class ExternalRequest
 {
     private WrappedRequest $request;
 
     private WrappedResponse $response;
 
+    private DateTimeImmutable $requestDate;
+
     public function __construct(
-        WrappedRequest  $request,
-        WrappedResponse $response
+        WrappedRequest     $request,
+        WrappedResponse    $response,
+        ?DateTimeImmutable $requestDate = null
     ) {
-        $this->request  = $request;
-        $this->response = $response;
+        $this->request     = $request;
+        $this->response    = $response;
+        $this->requestDate = $requestDate ?? new DateTimeImmutable();
     }
 
     public function request(): WrappedRequest
@@ -26,5 +32,10 @@ class ExternalRequest
     public function response(): WrappedResponse
     {
         return $this->response;
+    }
+
+    public function requestDate(): DateTimeImmutable
+    {
+        return $this->requestDate;
     }
 }
--- tests/Unit/Logger/ExternalRequestTest.php
+++ tests/Unit/Logger/ExternalRequestTest.php
@@ -7,6 +7,7 @@ namespace Bmwx591\RequestLogger\Tests\Unit\Logger;
 use Bmwx591\RequestLogger\Logger\ExternalRequest;
 use Bmwx591\RequestLogger\Logger\WrappedRequest;
 use Bmwx591\RequestLogger\Logger\WrappedResponse;
+use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
 
 class ExternalRequestTest extends TestCase
@@ -18,12 +19,15 @@ class ExternalRequestTest extends TestCase
     {
         $wrappedRequest  = $this->createStub(WrappedRequest::class);
         $wrappedResponse = $this->createStub(WrappedResponse::class);
+        $now = new DateTimeImmutable('+2 sec');
 
-        $externalRequest = new ExternalRequest($wrappedRequest, $wrappedResponse);
+        $externalRequest = new ExternalRequest($wrappedRequest, $wrappedResponse, $now);
 
         self::assertInstanceOf(WrappedRequest::class, $externalRequest->request());
         self::assertInstanceOf(WrappedResponse::class, $externalRequest->response());
+        self::assertInstanceOf(DateTimeImmutable::class, $externalRequest->requestDate());
         self::assertSame($wrappedRequest, $externalRequest->request());
         self::assertSame($wrappedResponse, $externalRequest->response());
+        self::assertSame($now, $externalRequest->requestDate());
     }
 }
